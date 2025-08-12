473ae934c8b1f7f439d49d58239027afb83afb82
v-thiyagarajan-1703 <v.thiyagarajan1703@gmail.com>
2025-08-12 13:55:20 +0530

    new

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "action170"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
